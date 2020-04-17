5e81107dffc11fc9b89446246285151df46f167f
AlbedoUniversal <Albedogforce@gmail.com>
2020-04-17 16:04:39 +0300

    reactTutorial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div className="app-wrapper container">
+      <Header />
+      <Navbar />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://clipartart.com/images/clipart-logo-editor-2.png"></img>
+    </header>
+  );
+};
+
+export default Header;
