74d7aff2d06ca209924a14045a3c63aea05e79ea
AlbedoUniversal <rybalko@ucoz-team.net>
2020-06-05 16:51:55 +0300

    сделал вот это

--- src/components/Dialogs/DialogsContainer.js
+++ src/components/Dialogs/DialogsContainer.js
@@ -4,50 +4,55 @@ import {
   updateNewMessageTextActionCreator
 } from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
-import connect from "react-redux";
+import {connect} from "react-redux";
 
-const DialogsContainer = props => {
+// const DialogsContainer = props => {
 
-let state = props.store.getState();
+// let state = props.store.getState();
 
-  let addMessage = () => {
-	props.store.dispatch(addMessageActionCreator());
-  };
+//   let addMessage = () => {
+// 	props.store.dispatch(addMessageActionCreator());
+//   };
 
-  let onMessageChange = text => {
-	let action = updateNewMessageTextActionCreator(text)
-    props.store.dispatch(action);
-  };
+//   let onMessageChange = text => {
+// 	let action = updateNewMessageTextActionCreator(text)
+//     props.store.dispatch(action);
+//   };
 
-  return (
-	  <Dialogs  addMessage={addMessage}
-	  			updateNewMessageText={onMessageChange}
-				dialogs={state.dialogsPage.dialogs}
-				messages={state.dialogsPage.messages}
-				newMessageText={state.dialogsPage.newMessageText}
-	   />
-  );
-};
+//   return (
+// 	  <Dialogs  addMessage={addMessage}
+// 	  			updateNewMessageText={onMessageChange}
+// 				dialogs={state.dialogsPage.dialogs}
+// 				messages={state.dialogsPage.messages}
+// 				newMessageText={state.dialogsPage.newMessageText}
+// 	   />
+//   );
+// };
 
 const mapStateToProps = (state) => {
 	return {
-		dialogsPage: state.dialogsPage
+		dialogs: state.dialogsPage.dialogs,
+		messages: state.dialogsPage.messages,
+		newMessageText: state.dialogsPage.newMessagesText
 	}
 }
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		updateNewMessageText: () => {
-
-		},
 		addMessage: () => {
 			dispatch(addMessageActionCreator());
+		},
+
+		updateNewMessageText: (text) => {
+			let action = updateNewMessageTextActionCreator(text);
+			dispatch(action);
+
 		}
 	}
 }
 
 
 
-const DialogsContainer = connect(mapStateToProps, mapDispatchToProps )(Dialogs);
+const DialogsContainer = connect(mapStateToProps, mapDispatchToProps)(Dialogs);
 
 export default DialogsContainer;
--- src/components/Profile/MyPosts/MyPostsContainer.js
+++ src/components/Profile/MyPosts/MyPostsContainer.js
@@ -4,30 +4,54 @@ import {
   addPostActionCreator
 } from "../../../redux/profile-reducer";
 import MyPosts from "./MyPosts";
+import { connect } from "react-redux";
 
-const MyPostsContainer = props => {
-  // в React мы используем virtual dom, поэтому мы не можем напрямую использовать все document.qetElem и так далее. Нам нужно создавать ссылку на элемент внутри компонента
-  // спец переменная которая будет содержать ссылку на элемеент(current ссылается на нативный html элемент)
+// const MyPostsContainer = props => {
+//   // в React мы используем virtual dom, поэтому мы не можем напрямую использовать все document.qetElem и так далее. Нам нужно создавать ссылку на элемент внутри компонента
+//   // спец переменная которая будет содержать ссылку на элемеент(current ссылается на нативный html элемент)
 
-  let state = props.store.getState();
+//   let state = props.store.getState();
 
-  const addPost = () => {
-    props.store.dispatch(addPostActionCreator());
-  };
+//   const addPost = () => {
+//     props.store.dispatch(addPostActionCreator());
+//   };
 
-  const onPostChange = text => {
-    let action = updateNewPostTextActionCreator(text);
-    props.store.dispatch(action);
-  };
+//   const onPostChange = text => {
+//     let action = updateNewPostTextActionCreator(text);
+//     props.store.dispatch(action);
+//   };
 
-  return (
-    <MyPosts
-      updateNewPostText={onPostChange}
-      posts={state.profilePage.posts}
-      addPost={addPost}
-      newPostText={state.profilePage.newPostText}
-    />
-  );
-};
+//   return (
+//     <MyPosts
+//       updateNewPostText={onPostChange}
+//       posts={state.profilePage.posts}
+//       addPost={addPost}
+//       newPostText={state.profilePage.newPostText}
+//     />
+//   );
+// };
+
+const mapStateToProps = (state) => {
+	return {
+		posts: state.profilePage.posts,
+		newPostText: state.profilePage.newPostText
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		updateNewPostText: (text) => {
+			let action = updateNewPostTextActionCreator(text);
+    		dispatch(action);
+		},
+
+		addPost: () => {
+			dispatch(addPostActionCreator());
+		}
+	}
+}
+
+
+const MyPostsContainer = connect(mapStateToProps, mapDispatchToProps)(MyPosts)
 
 export default MyPostsContainer;
